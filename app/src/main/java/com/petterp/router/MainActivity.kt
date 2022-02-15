42944bd08b879b584b51e20c729864763e4aa5f2
petterp <shiyihuicloud@163.com>
2022-02-16 01:14:44 +0800

    feat:移除测试文件

--- app/src/main/java/com/petterp/router/MainActivity.kt
+++ app/src/main/java/com/petterp/router/MainActivity.kt
@@ -3,13 +3,11 @@ package com.petterp.router
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.petterp.router.annotations.Router
-import com.petterp.router.mapping.RouterMapping_1644941330530
 
 @Router("router://home", "应用主页")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var mapping = RouterMapping_1644941330530.getMapping()
     }
 }
